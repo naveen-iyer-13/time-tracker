3e0685d516aa1eb0eb6598f98a6028f113f0f0a1
Naveen <naveeniyer13@gmail.com>
2018-09-22 12:56:31 +0530

    Setup AWS s3 deployment

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,14 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "dist/",
+    path: path.resolve(__dirname, "public/"),
+    publicPath: "public/",
     filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3001,
-    publicPath: "http://localhost:3001/dist/",
+    publicPath: "http://localhost:3001/public/",
     hotOnly: true
   },
   plugins: [ new webpack.HotModuleReplacementPlugin() ]
